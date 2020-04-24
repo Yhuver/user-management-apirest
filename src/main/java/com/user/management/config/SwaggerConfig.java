c0cedf3ffd735aaada81e06aaf4778a4abc61fb2
Yhuver Quintero <yhuver@gmail.com>
2020-04-24 11:05:20 -0500

    Implementando swagger para visualización de endpoints

---
+++ src/main/java/com/user/management/config/SwaggerConfig.java
@@ -0,0 +1,68 @@
+package com.user.management.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.user.management.controller"))              
+          .paths(PathSelectors.any())                          
+          .build()
+          .apiInfo(apiInfo());
+    }	
+	
+	private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "REST API GENERAL",
+                "Some custom description of API.",
+                "API BACKEND-GENERAL",
+                "Terms of service",
+                new Contact("Yhuver Andrey Quintero", "", "yhuver@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+	
+	@Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
+    }
+
+}
